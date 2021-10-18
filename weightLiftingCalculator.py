3995aa447f4c3f25efb3a792f73c8730e8d909e8
Vicky Yu <44479594+vickyLikesCoding@users.noreply.github.com>
2021-10-18 10:25:46 -0700

    Create weightLiftingCalculator.py

---
+++ weightLiftingCalculator.py
@@ -0,0 +1,10 @@
+pounds = int(input("Which plate did you add to one side of the bar? (in lbs)")
+totalPounds = (pounds * 2) + 45
+addWeight = input("Would you like to add more weight? (yes or no)")
+
+if (addWeight == "yes")
+  morePounds = int(input("Which plate did you add to one side of the bar? (in lbs)")
+  totalPounds += (morePounds * 2)
+  addWeight = input("Would you like to add more weight? (yes or no)")
+else
+  print("your total weight was:" totalPounds)
